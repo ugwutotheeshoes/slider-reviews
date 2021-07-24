04eb5209e1a3318d49d846f4aa4799924da8fd90
Emmanuel Ugwu <>
2021-07-24 16:05:06 +0100

    New changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import People from './People';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+     const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex){
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000);
+    return () => clearInterval(slider)
+  }, [index])
     
   return (
   <section className="section">
   <div className="title">
   <h2>
-  <span>/</span>reviews
+  reviews
   </h2>
+  <div className="underline"></div>
   </div>
   <div className="section-center">
   {people.map((person, personIndex) => {
-        return <People key={person.id} {...person}></People>
+        return <People key={person.id} {...person} personIndex={personIndex} index={index} setIndex={setIndex}></People>
   })}
-  <button className="prev">
+  <button className="prev" onClick={() => setIndex(index - 1)}>
     <FiChevronLeft />
   </button>
-  <button className="next">
+  <button className="next" onClick={() => setIndex(index + 1)}>
     <FiChevronRight />
   </button>
   </div>
--- src/People.js
+++ src/People.js
@@ -1,10 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { FaQuoteRight } from 'react-icons/fa';
+import people from './data';
 
-const People = ({id, image, name, title, quote}) => {
-    const [index, setIndex] = useState(0);
+const People = ({id, image, name, title, quote, personIndex, index, setIndex}) => {
+    
+    let position = "nextSlide";
+    if(personIndex === index){
+        position = 'activeSlide'
+    }
+    if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
+        position = 'lastSlide'
+    }
     return(
-        <article key={id}>
+        <article className={position} key={id}>
                 <img src={image} alt={name} className="person-img" />
                 <h4>{name}</h4>
                 <p className="title">{title}</p>
