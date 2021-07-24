1df0dfdc1ea2b2c4f12827ee64734942a682867a
Emmanuel Ugwu <>
2021-07-24 13:24:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  return (
+  <section className="section">
+  <div className="title">
+  <h2>
+  <span>/</span>reviews
+  </h2>
+  </div>
+  </section>
+  );
 }
 
 export default App;
